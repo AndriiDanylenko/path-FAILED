e5a3af0991c136d392800e88a7d5660f5457603b
dvd-ai <56834006+dvd-ai@users.noreply.github.com>
2020-09-10 17:06:45 +0300

    push_back_island upd

--- src/store_push_back_island.c
+++ src/store_push_back_island.c
@@ -1,30 +1,26 @@
 #include "findpath.h"
 #include "../inc/libmx.h"
 
-static int island_amount(island_list *head) {
-    int counter = 0;
+// static int island_amount(island_list *head) {
+//     int counter = 0;
     
-    if (!head)
-        return 0;
+//     if (!head)
+//         return 0;
 
-    island_list *current = head;
+//     island_list *current = head;
 
-    while(current) {
-        current = current->next;
-        counter++;
-    }
-    return counter;
-}
+//     while(current) {
+//         current = current->next;
+//         counter++;
+//     }
+//     return counter;
+// }
 
 void store_push_back_island(island_list **head, char *name, int number) {
     if (!*head && head) {
         *head = store_create_island(name, number);
         return;
     } 
-    
-    if (island_amount(*head) == 1) {
-        (*head)->previous = NULL;
-    }
     
     island_list *current_island = *head;
     island_list *back_island = store_create_island(name, number);
